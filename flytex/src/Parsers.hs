966875ff6f73f5a11c644bf6d9cc889d4cc4fc44
indrjo <dedej.indrjo@gmail.com>
2023-12-16 10:05:45 +0100

    two new fucntions, `listCapturesParser` and `listCaptures`, do the the dirt work that deserves its own name; some comments added.

--- flytex/src/Parsers.hs
+++ flytex/src/Parsers.hs
@@ -15,22 +15,14 @@ import Text.Parsec.String
 -- From the output of `tlmgr search --global --file PATTERN` list the names
 -- the names of the packages that occur. 
 listPackageNames :: String -> [String]
-listPackageNames = either (\_ -> []) id . parse packageNamesParser ""
+listPackageNames = listCaptures packageNameParser
 
 -- From "\nNAME:\n" extract "NAME".
-packageNameParser :: GenParser Char s String
+packageNameParser ::  Parser String
 packageNameParser =
   between newline
           (char ':' >> newline)
           (many1 $ noneOf " :")
-
--- The parser of the output of `tlmgr search --global --file PATTERN`.
-packageNamesParser :: GenParser Char s [String]
-packageNamesParser =
-      try (packageNameParser >>=
-            \name -> (name :) <$> packageNamesParser)
-  <|> try (anyChar >> packageNamesParser)
-  <|> return []
 
 -- Capture the names the missing files from the log of a TeX command. More
 -- precisely, look for pieces of the form 
@@ -39,27 +31,36 @@ packageNamesParser =
 -- 
 -- and isolate FILE.
 listNotFounds :: String -> [String]
-listNotFounds = either (\_ -> []) id . parse notFoundsParser ""
+listNotFounds = listCaptures notFoundParser
 
 quoteMarks :: String
 quoteMarks = "'\"`"
 
 -- Extract the fragment between two quote marks.
-quotedNameParser :: GenParser Char s String
+quotedNameParser :: Parser String
 quotedNameParser =
   between (oneOf quoteMarks)
           (oneOf quoteMarks)
           (many1 $ noneOf quoteMarks)
 
 -- From a string complaining of something not found give its name.
-notFoundParser :: GenParser Char s String
+notFoundParser :: Parser String
 notFoundParser = quotedNameParser <* (spaces >> string "not found")
 
--- The parser that isolates the missing files from the log.
-notFoundsParser :: GenParser Char s [String]
-notFoundsParser =
-      try (notFoundParser >>=
-            \name -> (name :) <$> notFoundsParser)
-  <|> try (anyChar >> notFoundsParser)
+
+-- Some helper functions used in this module.
+
+-- While consuming the input string, try a given parser: if it succeeds,
+-- collect the match into a list, otherwise move on by one character.
+listCapturesParser :: Parser String -> Parser [String]
+listCapturesParser p =
+      try (p >>=
+            \str -> (str :) <$> listCapturesParser p)
+  <|> try (anyChar >> listCapturesParser p)
   <|> return []
 
+-- Since `parse (listCapturesParser p) ""` never gives a `Left ParseError`,
+-- it safe to expect always the `Right` value.
+listCaptures :: Parser String -> String -> [String]
+listCaptures p =
+  either (\_ -> []) id . parse (listCapturesParser p) ""
