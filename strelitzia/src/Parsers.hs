19e90228d36cdca023b4b41109f34c6ee148de8d
indrjo <dedej.indrjo@gmail.com>
2024-03-03 15:09:43 +0100

    code reorg

--- strelitzia/src/Parsers.hs
+++ strelitzia/src/Parsers.hs
@@ -9,11 +9,14 @@ import Text.Parsec.String
 -- DESCRIPTION
 --
 -- This module defines some functions that take a string as argument (the
--- contents of the standard output or some log) and to extract some names.
+-- contents of the standard output or some log) and to extract some names
+-- with aid of parsers.
 --
 
--- From the output of `tlmgr search --global --file PATTERN` list the names
--- the names of the packages that occur. 
+-- ------------------------------------------------------------------------
+-- Parse the output of `tlmgr search --global --file PATTERN`.
+-- ------------------------------------------------------------------------
+
 listPackageNames :: String -> [String]
 listPackageNames = listCaptures packageNameParser
 
@@ -22,45 +25,50 @@ packageNameParser ::  Parser String
 packageNameParser =
   between newline
           (char ':' >> newline)
-          (many1 $ noneOf " :")
+          (many1 $ noneOf ":")
 
--- Capture the names the missing files from the log of a TeX command. More
--- precisely, look for pieces of the form 
+
+-- ------------------------------------------------------------------------
+-- Extract the names of the missing file from the log of the compilation.
+-- ------------------------------------------------------------------------
+
+-- So far the parser looks for pieces having the following pattern
 --
---   "FILE" not found
--- 
--- and isolate FILE.
+--   <quote-mark>FILE<quote-mark> not found
+--
+-- and isolates just FILE. See `quoteMarks` for a list of quotation marks
+-- used to wrap the name of the missing.
 listNotFounds :: String -> [String]
 listNotFounds = listCaptures notFoundParser
+
+-- From a string complaining of something not found of the fashion shown
+-- above isolate the name.
+notFoundParser :: Parser String
+notFoundParser =
+  between (oneOf quoteMarks)
+          (oneOf quoteMarks >> spaces >> string "not found")
+          (many1 $ noneOf quoteMarks)
 
 quoteMarks :: String
 quoteMarks = "'\"`"
 
--- Extract the fragment between two quote marks.
-quotedNameParser :: Parser String
-quotedNameParser =
-  between (oneOf quoteMarks)
-          (oneOf quoteMarks)
-          (many1 $ noneOf quoteMarks)
 
--- From a string complaining of something not found give its name.
-notFoundParser :: Parser String
-notFoundParser = quotedNameParser <* (spaces >> string "not found")
-
-
--- Some helper functions used in this module.
+-- ------------------------------------------------------------------------
+-- Helper functions.
+-- ------------------------------------------------------------------------
 
 -- While consuming the input string, try a given parser: if it succeeds,
--- collect the match into a list, otherwise move on by one character.
+-- collect the match into a list, otherwise move on by one char and retry
+-- until the input is wholly consumed.
 listCapturesParser :: Parser String -> Parser [String]
 listCapturesParser p =
-      try (p >>=
-            \str -> (str :) <$> listCapturesParser p)
+      try (p >>= \str ->
+                   (str :) <$> listCapturesParser p)
   <|> try (anyChar >> listCapturesParser p)
   <|> return []
 
 -- Since `parse (listCapturesParser p) ""` never gives a `Left ParseError`,
 -- it safe to expect always the `Right` value.
 listCaptures :: Parser String -> String -> [String]
-listCaptures p =
-  either (\_ -> []) id . parse (listCapturesParser p) ""
+listCaptures p = either (\_ -> []) id . parse (listCapturesParser p) ""
+
--- strelitzia/src/Tlmgr.hs
+++ strelitzia/src/Tlmgr.hs
@@ -5,21 +5,34 @@ import Data.List (intercalate)
 import RunShell
 import Parsers (listPackageNames)
 
+
 -- DESCRIPTION
 --
 -- A minimal TeX Live has *tlmgr* to handle packages: not only you install
 -- packages with it, but you can search packages containing a given file!
 --
 
--- Take a list of packages and install them.
-tlmgrInstall :: [String] -> IO ()
-tlmgrInstall packages =
-  callCommand $ "tlmgr install " ++ unwords packages
+-- ------------------------------------------------------------------------
+-- Search for packages.
+-- ------------------------------------------------------------------------
 
 -- Take a list of files not found and list the packages containing them.
 tlmgrSearch :: [String] -> IO [String]
 tlmgrSearch notFounds =
-  let searchPattern = "'/(" ++ intercalate "|" notFounds ++ ")'" in
-    listPackageNames <$>
-      runShell ("tlmgr search --global --file " ++ searchPattern) ""
+  listPackageNames <$> runShell ("tlmgr search --global --file "
+                                 ++ searchPattern notFounds) ""
+
+searchPattern :: [String] -> String
+searchPattern []     = ""
+searchPattern [name] = "'/" ++ name ++ "'"
+searchPattern names  = "'/(" ++ intercalate "|" names ++ ")'"
+
+
+-- ------------------------------------------------------------------------
+-- Install packages.
+-- ------------------------------------------------------------------------
+
+tlmgrInstall :: [String] -> IO ()
+tlmgrInstall packages =
+  callCommand $ "tlmgr install " ++ unwords packages
 
