8eb252d791f194ec72fa5edd3f6596f60646fd66
indrjo <dedej.indrjo@gmail.com>
2024-03-15 17:52:05 +0100

    added `wgetSpider`; small refactor

--- strelitzia/src/RunShell.hs
+++ strelitzia/src/RunShell.hs
@@ -1,13 +1,32 @@
 
-module RunShell (runShell, callCommand) where
+module RunShell ( runShell
+                , callCommand
+                , wgetSpider
+                ) where
 
 import System.Process (readCreateProcessWithExitCode, shell, callCommand)
+import System.Exit (ExitCode(..))
 
--- Make the host OS run some command, and return all the output in a unique
--- string. This is achieved by appending "2>&1" at the end of the command.
--- The second argument is a string to be passed to the stdin if the command
--- requires user interaction somewhere.
+-- Make the host OS run some command, and return all its output as a unique
+-- string and the the exit status. The second argument is a string to be
+-- passed to the stdin if the command requires user interaction somewhere.
+readShellCommand :: String -> String -> IO (ExitCode, String)
+readShellCommand comm inpstr =
+  let comm' = shell $ unwords [comm, "2>&1"] in do
+    (exitcode, allout, _) <- readCreateProcessWithExitCode comm' inpstr
+    return (exitcode, allout)
+
 runShell :: String -> String -> IO String
-runShell comm instr = (\(_, out, _) -> out) <$>
-  readCreateProcessWithExitCode (shell $ comm ++ " 2>&1") instr
+runShell comm inpstr = do
+  (_, out) <- readShellCommand comm inpstr
+  return out
+
+quote :: String -> String
+quote str = "'" ++ str ++ "'"
+
+-- Check if you can reach a given url.
+wgetSpider :: String -> IO Bool
+wgetSpider url = do
+  (exitcode, _) <- readShellCommand ("wget -q --spider " ++ quote url) ""
+  return $ exitcode == ExitSuccess
 
